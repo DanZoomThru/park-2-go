caa2e8fee19844cba8c8f9afb068e7ce128c5317
Dan <danerictech@gmail.com>
2016-10-25 14:52:45 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
 	<meta name="apple-mobile-web-app-capable" content="yes">
 	<meta name="apple-mobile-web-app-status-bar-style" content="black">
-	<meta name="apple-mobile-web-app-title" content="Park and Go London">
+	<meta name="apple-mobile-web-app-title" content="Park and Go in London by ZoomThru">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no, minimal-ui">
     <title>ParkAndGo</title>
     <style type="text/css">
